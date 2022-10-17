93c7c0ed63e88fb94139cedf1c5551d0f84a073c
Jeff Raker <86754427+jraker01@users.noreply.github.com>
2022-10-17 18:51:18 -0400

    Create initial template file

--- r_code_template.r
+++ r_code_template.r
@@ -1,1 +1,35 @@
+################################################################################
+## TITLE                                                                      ##
+##                                                                            ##
+## Based on [include as appropriate]                                          ##
+################################################################################
 
+## Author: [NAME]
+## Released: [YYYY]
+## Last Updated: [YYYY-MM-DD]
+
+################################################################################
+
+### THIS SECTION IS FOR LOADING NECESSARY PACKAGES ###
+
+## Install packages (and dependencies) if necessary
+# install.packages("[INSERT]", dependencies = TRUE)
+
+## The "[INSERT]" package is used to [DESCRIBE]
+library ([INSERT])
+
+################################################################################
+
+### THIS SECTION CREATES/DOES [INSERT] ###                   # Repeat as needed.
+
+## [Insert specific description]
+[INSERT code]
+
+################################################################################
+
+### THIS SECTION EXPORTS/SAVES/ETC. ###
+
+## [Insert specific description]
+[INSERT code]
+
+################################################################################
